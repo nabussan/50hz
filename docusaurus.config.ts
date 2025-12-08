a349bf0e3c1f90cbeb295e5fa9b9054fbb809c44
Christoph Albrecht <ch.albrecht@posteo.de>
2025-12-08 13:00:04 +0100

    Update site URL and base URL in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://philstollery.github.io',
+  url: 'https://nabussan.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/github-usaurus/',
+  baseUrl: '/50hz/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
